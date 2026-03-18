b1bedce37dee8bdf9dd7c354d1ea81f48e45ce52
meledych1111-ux <meledych1111@gmail.com>
2026-03-18 10:21:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,63 +1,17 @@
 const CACHE_NAME = 'img-converter-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
 
-// Установка и кэширование
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Активация и очистка старых кэшей
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(['/', '/index.html', '/manifest.json', '/icon.png']);
     })
   );
 });
 
-// Перехват запросов
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Возвращаем из кэша если есть
-        if (response) {
-          return response;
-        }
-        
-        // Иначе запрашиваем из сети
-        return fetch(event.request).then(
-          response => {
-            // Не кэшируем если не валидный ответ
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            
-            // Клонируем и кэшируем
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-            
-            return response;
-          }
-        );
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
